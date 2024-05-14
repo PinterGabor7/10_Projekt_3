c864060009310e0d88ef4030aed25f7e7ea22978
Rozs Barbara <rozs.barbara@students.jedlik.eu>
2024-05-14 12:29:20 +0200

    python 3 statisztika

--- asztalitenisz.py
+++ asztalitenisz.py
@@ -1,8 +1,10 @@
 class Jatekos:
 
     def __init__(self, sor: str):
-        adatok = sor.strip().split()
+        adatok = sor.strip().split(';')
         self.nev = adatok[0]
-        self.szuletes = int(adatok[1])
-        self.nyert = int(adatok[2])
-        self.vesztett = int(adatok[3])
+        self.nem = adatok[1]
+        self.szarmazas = adatok[2]
+        self.szuletes = int(adatok[3])
+        self.nyert = int(adatok[4])
+        self.vesztett = int(adatok[5])
--- jatekosok.py
+++ jatekosok.py
@@ -4,7 +4,30 @@ jatekosok: list[Jatekos] = []
 
 def main():
     beolvasas() 
-    print(f'2. feladat: A fájlban {len(jatekosok)} adata szerepel.')
+    print(f'2. feladat: A fájlban {len(jatekosok)} játékos adata szerepel.')
+    jatekos_kereses()
+    jatekosok_szama()
+
+def jatekos_kereses():
+    jatekos = input('\nKérem a keresett játékos nevét: ')
+    i = 0
+    while i < len(jatekosok) and jatekos != jatekosok[i].nev:
+        i += 1
+    if i < len(jatekosok):
+        print(f'\t{jatekosok[i].nev} {jatekosok[i].nyert} versenyt nyert.')
+    else: 
+        print('\tNincs ilyen versenyző!')
+
+def jatekosok_szama():
+    orszagok = {}
+    for j in jatekosok:
+        orszag = j.szarmazas
+        if orszag in orszagok:
+            orszagok[orszag] += 1
+        else:
+            orszagok[orszag] = 1
+    for orszag, jatekosok_szama in orszagok.items():
+        print(f'\t{orszag}: {jatekosok_szama} fő')
 
 def beolvasas():
     f = open('jatekosok.txt', 'r', encoding='utf-8')
@@ -12,10 +35,5 @@ def beolvasas():
     for sor in f:
         jatekosok.append(Jatekos(sor))
     f.close()
-
-
-
-
-
 
 main()
